81603c4ac2ead946c5b72862d05d436cf6acd6a3
Jeyasriganesh <srigirija003@gmail.com>
2025-01-01 16:11:27 +0530

    Initial commit of Laravel project

---
+++ book-reviews/app/Filament/Resources/BookReviewResource/Pages/CreateBookReview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\BookReviewResource\Pages;
+
+use App\Filament\Resources\BookReviewResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBookReview extends CreateRecord
+{
+    protected static string $resource = BookReviewResource::class;
+
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
+}
